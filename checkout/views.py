0ad477029dca82c653cd78c09fcabf93aa6ec735
naragurgel <gurgel.nara@gmail.com>
2023-07-18 12:35:07 +0000

    setting up stripe

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51NVBCUHBtKgjniPA2LQd4YeOaUEBGYyKmH7CLXohSJdy8gI3GDpoKSJYIYbvqCG1gRbISfWQJ8qDh9kmXMSGj0Qo00CZbhnLmJ',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
