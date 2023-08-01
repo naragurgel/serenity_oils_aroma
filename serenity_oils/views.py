3c6aa3cec5ceba31cf6d1fbfb89b1494b3dd3161
naragurgel <gurgel.nara@gmail.com>
2023-08-01 14:53:37 +0000

    created page error views

--- serenity_oils/views.py
+++ serenity_oils/views.py
@@ -4,3 +4,18 @@ from django.shortcuts import render
 def handler404(request, exception):
     """ Error Handler 404 - Page Not Found """
     return render(request, "errors/404.html", status=404)
+
+
+def handler403(request, exception):
+    """ Error Handler 403 - Forbidden Error Variations """
+    return render(request, "errors/403.html", status=403)
+
+
+def handler500(request):
+    """ Error Handler 500 - Server Error """
+    return render(request, "errors/500.html", status=500)
+
+
+def handler400(request, exception):
+    """ Error Handler 400 - Bad Request """
+    return render(request, "errors/400.html", status=400)
