a4dcd75881501c4b61018ecd7fbd31573012e9fb
naragurgel <gurgel.nara@gmail.com>
2023-08-03 18:41:11 +0000

    updated templates

--- testimonials/urls.py
+++ testimonials/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("testimonials", views.testimonial_list, name="testimonials"),
+    path("", views.testimonial_list, name="testimonials"),
     path('add/', views.add_testimonial, name='add_testimonial'),
     path('edit/<slug:slug>/', views.edit_testimonial, name='edit_testimonial'),
     path('delete/<slug:slug>/', views.delete_testimonial, name='delete_testimonial'),
