59f9df620e7609f277eb199bba36f136a817b74b
naragurgel <gurgel.nara@gmail.com>
2023-08-08 08:18:17 +0000

    set up project's email

--- serenity_oils/settings.py
+++ serenity_oils/settings.py
@@ -223,9 +223,13 @@ STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'serenity.oils.ng@gmail.com'
-EMAIL_HOST_PASSWORD = 'project5serenity'
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'serenity.oils.ng@gmail.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST_USER = 'serenity.oils.ng@gmail.com'
+    EMAIL_HOST_PASSWORD = 'project5serenity'
