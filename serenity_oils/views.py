5850f335f40bbcc884639f8a7b7fbb126893f51a
naragurgel <gurgel.nara@gmail.com>
2023-08-08 08:26:56 +0000

    fixing email bug

--- serenity_oils/views.py
+++ serenity_oils/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from django.core.mail import send_mail
-from django.conf import settings
 
 
 def handler404(request, exception):
@@ -21,12 +19,3 @@ def handler500(request):
 def handler400(request, exception):
     """ Error Handler 400 - Bad Request """
     return render(request, "errors/400.html", status=400)
-
-
-def email(request):
-    subject = 'Thank you for registering to our site'
-    message = ' it  means a world to us '
-    email_from = settings.EMAIL_HOST_USER
-    recipient_list = ['receiver@gmail.com',]
-    send_mail(subject, message, email_from, recipient_list)
-    return redirect('redirect to a new page')
