c9ed40b4b87e58d93028ee573258c34e07dc3654
naragurgel <gurgel.nara@gmail.com>
2023-08-10 20:46:08 +0000

    deleted froms from blog

--- blog/forms.py
+++ blog/forms.py
@@ -1,9 +1,1 @@
-from .models import Comment
 from django import forms
-
-
-class CommentForm(forms.ModelForm):
-    # Define a form for creating and updating comments
-    class Meta:
-        model = Comment
-        fields = ('body',)
