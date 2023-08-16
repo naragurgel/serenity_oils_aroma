a9604f4c6ae97c0a4d0deda558f520f9be21b6d1
naragurgel <gurgel.nara@gmail.com>
2023-08-16 18:06:23 +0000

    added error pages templates

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404  # noqa
 from django.contrib import messages
 
 from products.models import Product
@@ -44,7 +44,7 @@ def adjust_bag(request, item_id):
 
     if quantity > 0:
         bag[item_id] = quantity
-        messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
+        messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')  # noqa
     else:
         bag.pop(item_id)
         messages.success(request, f'Removed {product.name} from your bag.')
