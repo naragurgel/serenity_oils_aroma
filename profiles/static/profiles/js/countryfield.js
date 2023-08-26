b9cfa0a28caa97d677ef2a91a7f014c86dba6133
naragurgel <gurgel.nara@gmail.com>
2023-08-26 15:39:11 +0000

    merge

--- profiles/static/profiles/js/countryfield.js
+++ profiles/static/profiles/js/countryfield.js
@@ -3,7 +3,7 @@ if(!countrySelected) {
     $('#id_default_country').css('color', '#aab7c4');
 };
 $('#id_default_country').change(function() {
-    countrySelected = $(this).val();
+    countrySelected = $(this).val()
     if(!countrySelected) {
         $(this).css('color', '#aab7c4');
     } else {
